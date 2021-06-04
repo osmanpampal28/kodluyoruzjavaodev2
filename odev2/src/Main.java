6063a1e46b3710343647bca034ed033e183359e7
osmanpampal28 <55186715+osmanpampal28@users.noreply.github.com>
2021-06-04 18:05:13 +0300

    2. ödev
    Kodluyoruz java patika 2. ödev

---
+++ odev2/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double tutar,kdvOran=0,kdvTutar,kdvliTutar;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("ücret tutarını giriniz : ");
+        tutar=scanner.nextDouble();
+        while(tutar<0){
+            System.out.println("tutar değeri 0'dan küçük olamaz! yeni değer giriniz : ");
+            tutar=scanner.nextDouble();
+        }
+
+        if(tutar>=1000){
+            kdvOran=0.08;
+        }else if(tutar<1000 && tutar>0){
+            kdvOran=0.18;
+        }
+
+        kdvTutar=tutar*kdvOran;
+        kdvliTutar=tutar+kdvTutar;
+
+        System.out.println("KDV'siz tutar : "+tutar);
+        System.out.println("KDV Orani : "+kdvOran);
+        System.out.println("KDV Tutari : "+kdvTutar);
+        System.out.println("KDV'li tutar : "+kdvliTutar);
+    }
+}
